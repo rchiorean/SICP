7045ebcb2ebe45bf294860f3ec1b032d566c12e6
Radu Chiorean <rchiorean@gmail.com>
2016-08-05 08:06:50 +0100

    Exercise 35

--- 1.3/ex1.35.scm
+++ 1.3/ex1.35.scm
@@ -1,2 +1,14 @@
 ; Dividing the golden ratio function by x results in x = 1 + 1/x. 
 ; TODO: implement fixed-point procedure
+(define (find-golden-ratio)
+  (define tolerance 0.0001)  
+  (define (close-enough? v1 v2)
+   (< (abs (- v2 v1)) tolerance)) 
+  (define (find-fixed-point f first-guess)
+   (define (try guess) 
+    (let ((next (f guess))) 
+      (if (close-enough? guess next) next (try next))))
+   (try first-guess))
+  (find-fixed-point (lambda (x) (+ 1.0 (/ 1.0 x))) 10))
+
+(find-golden-ratio)
